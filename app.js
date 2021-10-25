7b73cd9bc1a9002b40e044d859182966ac2ac0c5
ali-vx <amuhammad161@gmail.com>
2021-10-25 22:25:26 +0500

    added logic for drawing on grid

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let enteredSize = parseInt(prompt("Enter Grid Size: "));
 let pixels = [];
 const pixelGrid = document.querySelector(".pixel-grid");
-
-window.addEventListener("load", () => {
+const generateGrid = () => {
   for (let i = 0; i < enteredSize; i++) {
     for (let j = 0; j < enteredSize; j++) {
       pixels[i] = document.createElement("div");
@@ -10,7 +9,16 @@ window.addEventListener("load", () => {
       pixelGrid.appendChild(pixels[i]);
       console.log(enteredSize);
     }
-    pixels[i].classList.add("new-line");
     pixels[i].style.setProperty("--grid-size", enteredSize);
   }
+};
+window.addEventListener("load", generateGrid);
+
+pixelGrid.addEventListener("mouseover", function (e) {
+  if (e.buttons > 0) {
+    e.target.classList.add("black");
+  }
+});
+pixelGrid.addEventListener("click", function (e) {
+  e.target.classList.add("black");
 });
