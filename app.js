a13406f27f4cbecf5b5ad9f057b4fac0b433ac3b
ali-vx <amuhammad161@gmail.com>
2021-10-30 02:17:01 +0500

    added clear grid feature

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 let enteredSize = parseInt(prompt("Enter Grid Size: "));
 let pixels = [];
 const pixelGrid = document.querySelector(".pixel-grid");
+const clearBtn = document.querySelector(".clear-btn");
+
 const generateGrid = () => {
+  let a = 0;
   for (let i = 0; i < enteredSize; i++) {
     for (let j = 0; j < enteredSize; j++) {
-      pixels[i] = document.createElement("div");
-      pixels[i].classList.add("pixel");
-      pixelGrid.appendChild(pixels[i]);
-      console.log(enteredSize);
+      pixels[a] = document.createElement("div");
+      pixels[a].classList.add("pixel");
+      pixelGrid.appendChild(pixels[a]);
+      a++;
     }
-    pixels[i].style.setProperty("--grid-size", enteredSize);
+    pixels[a - 1].style.setProperty("--grid-size", enteredSize);
   }
 };
-window.addEventListener("load", generateGrid);
 
+const clearGrid = () => {
+  for (let el of pixels) {
+    if (el.classList.contains("black")) el.classList.remove("black");
+  }
+};
+
+clearBtn.addEventListener("click", clearGrid);
+window.addEventListener("load", generateGrid);
 pixelGrid.addEventListener("mouseover", function (e) {
   if (e.buttons > 0) {
     e.target.classList.add("black");
